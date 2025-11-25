79aac79db0e3c864ceeba7529b321bdaaab33844
Houngansi <aleks.vaal@gmail.com>
2025-11-25 00:27:04 -0500

    update locale

--- Source/BridgeSystem/MedComShared/Private/Interfaces/Weapon/IMedComWeaponAnimationInterface.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright MedCom Team. All Rights Reserved.
-
-#include "Interfaces/Weapon/IMedComWeaponAnimationInterface.h"
-
-// This file intentionally contains no implementation
-// All BlueprintNativeEvent methods will have their _Implementation 
-// generated automatically by Unreal Engine and must be implemented
-// in concrete classes that implement this interface
--- Source/BridgeSystem/MedComShared/Private/Types/Animation/AnimationStateStruct.cpp
+++
@@ -1,1 +0,0 @@
-#include "Types/Animation/AnimationStateStruct.h"
